2b3cabefeb6ead7746445ccd15bcc56a4d78c62a
sliman180 <sliman258@gmail.com>
2016-02-23 13:26:02 +0100

    Android

--- KandoeAndroid/app/src/main/java/be/kdg/kandoe/kandoeandroid/cirkelsessie/CirkelsessieActivity.java
+++
@@ -1,7 +0,0 @@
-package be.kdg.kandoe.kandoeandroid.cirkelsessie;
-
-/**
- * Created by S on 23-2-2016.
- */
-public class CirkelsessieActivity {
-}
--- KandoeAndroid/app/src/main/java/be/kdg/kandoe/kandoeandroid/kaart/KaartActivity.java
+++
@@ -1,7 +0,0 @@
-package be.kdg.kandoe.kandoeandroid.kaart;
-
-/**
- * Created by S on 23-2-2016.
- */
-public class KaartActivity {
-}
